7d5ad3dc0604b62cea9cb0fbaedba71d5636b1ce
Jiří Vrba <jiri@vrba.dev>
2020-03-27 16:38:25 +0100

    Hover animation product preview

--- src/components/ProductPreview.vue
+++ src/components/ProductPreview.vue
@@ -1,26 +1,39 @@
 <template>
     <v-col cols="12" sm="6" md="3" lg="2">
-        <v-img
-                :src="image"
-                aspect-ratio="1"
-                class="product--preview"
-                @click="$store.dispatch('viewProductDetails', slug)"
+        <v-hover
+                v-slot:default="{ hover }"
         >
-            <template v-slot:placeholder>
-                <v-row
-                        class="fill-height ma-0"
-                        align="center"
-                        justify="center"
-                >
-                    <v-progress-circular indeterminate color="primary"></v-progress-circular>
-                </v-row>
-            </template>
-        </v-img>
+            <v-img
+                    :elevation="hover ? 12 : 2"
+                    :src="image"
+                    aspect-ratio="1"
+                    class="product--preview"
+                    @click="$store.dispatch('viewProductDetails', slug)"
+            >
+                <v-fade-transition>
+                    <div v-if="hover"
+                            class="d-flex transition-fast-in-fast-out white v-card--reveal display-3 white--text"
+                            style="height: 100%; opacity: 0.25"
+                    >
+                    </div>
+                </v-fade-transition>
+                <template v-slot:placeholder>
+                    <v-row
+                            class="fill-height ma-0"
+                            align="center"
+                            justify="center"
+                    >
+                        <v-progress-circular indeterminate color="primary"></v-progress-circular>
+                    </v-row>
+                </template>
+            </v-img>
+        </v-hover>
     </v-col>
 </template>
 <style>
     .product--preview {
         border-radius: 5px;
+        cursor: pointer;
     }
 </style>
 <script>
